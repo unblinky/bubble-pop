bee05b9d34688983934fe657394e765687999e82
unblinky <47279470+unblinky@users.noreply.github.com>
2026-01-26 16:17:03 -0800

    Added SFX. Fixed memory leak.

--- Code/Bubble/Bubble.gd
+++ Code/Bubble/Bubble.gd
@@ -1,12 +1,26 @@
-extends Node2D
+extends Area2D
 class_name Bubble
 
+var main: Main
 var speed: float = -112 # px / sec.
 
-
 func _ready() -> void:
-	pass # Replace with function body.
-
+	input_event.connect(on_clicked)
 
 func _process(delta: float) -> void:
 	position.y += speed * delta
+	if position.y < -100:
+		queue_free()
+
+func on_clicked(_viewport, event, _shape):
+	if event is InputEventMouseButton:
+		print("A button was clicked.")
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			print("It's the Left Button!")
+			if event.pressed:
+				print("Mouse down.")
+				main.sfx.play()
+				main.sfx.pitch_scale = randf_range(0.8, 1.2)
+				queue_free()
+			else:
+				print("Mouse up.")
--- Code/Main/Main.gd
+++ Code/Main/Main.gd
@@ -2,11 +2,17 @@ extends Node
 class_name Main
 
 const BUBBLE = preload("res://Bubble/Bubble.tscn")
+@onready var timer: Timer = $Timer
+@onready var sfx: AudioStreamPlayer2D = $Sfx
 
 func _ready() -> void:
-	spawn_bubble()
+	timer.timeout.connect(on_timed_out)
 
 func spawn_bubble():
 	var bubble: Bubble = BUBBLE.instantiate()
-	bubble.position = Vector2(randi_range(0, get_viewport().size.x), get_viewport().size.y)
+	bubble.position = Vector2(randi_range(0, get_viewport().size.x), get_viewport().size.y + 100)
+	bubble.main = self
 	add_child(bubble)
+
+func on_timed_out():
+	spawn_bubble()
